ef17ce9cd3a46a2f84a661a6d0a456cf83a7873e
Your Name <you@example.com>
2019-07-23 16:31:34 -0700

    Updated landing page and allowed users to choose their own compliments

--- server.py
+++ server.py
@@ -18,7 +18,18 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+        <head>
+            <title>Home</title>
+        </head>
+        <body>
+            Hi! This is the home page.
+            <br>
+            <a href="/hello">Hello!</a>
+        </body>
+    </html>"""
 
 
 @app.route("/hello")
@@ -34,8 +45,17 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
+            What's your name? <input type="text" name="person">
+            <br>
+            Choose a compliment:
+            <select name="compliment">
+                <option>Pretty</option>
+                <option>Smart</option>
+                <option>Awesome</option>
+                <option>Badass</option>
+            </select>
+            <br>
+            <input type="submit" value="Submit">
         </form>
       </body>
     </html>
@@ -48,9 +68,9 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get('compliment')
 
-    y = x
+    # y = x
 
     return """
     <!doctype html>
@@ -68,4 +88,4 @@ def greet_person():
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
